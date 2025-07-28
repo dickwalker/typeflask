2bef3a18818aa23b635f5f6e3c002ef63e12cf68
airbornedev <richardwalkerjr@yahoo.com>
2025-07-28 08:19:30 -0500

    Removed empty/unused files.

--- routes.py
+++ routes.py
@@ -5,11 +5,3 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
 	return render_template("index.html")
-
-@bp.route('/pricing')
-def pricing():
-	return "Pricing"
-
-@bp.route('/about')
-def about():
-	return "About"
